2968ade5a3073b475514256738c0f05e70f41252
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-06-21 22:45:02 +0300

    initialization for redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './state';
+
 import { RootLayout } from './pages/RootLayout';
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './pages/PostDetails';
@@ -41,6 +44,8 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/state/postSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { records: [], loading: false, error: null };
+
+const postSlice = createSlice({
+  name: 'post',
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
+
+export default postSlice.reducer;
