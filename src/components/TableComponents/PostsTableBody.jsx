44c8881071eb817425466b4cd5dbdd7d070921cc
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-07-11 19:36:08 +0300

    Adding posted by section on the table

--- src/components/TableComponents/PostsTableBody.jsx
+++ src/components/TableComponents/PostsTableBody.jsx
@@ -25,6 +25,9 @@ export const PostsTableBody = ({ post, index }) => {
           </Link>
         </td>
         <td className="p-3 text-gray-700 text-md whitespace-nowrap">
+          {post.user}
+        </td>
+        <td className="p-3 text-gray-700 text-md whitespace-nowrap">
           <div className="flex items-center gap-2">
             <button
               className="px-2 py-[1px] hover:bg-green-500 duration-100 ease-in bg-green-600 rounded-md text-white"
