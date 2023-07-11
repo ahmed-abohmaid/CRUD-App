fa831abc1a92a4b11a426f58cdfc85d8b0757d5f
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-07-11 19:43:26 +0300

    Adding show/hide password icon

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { RiEyeLine, RiEyeOffLine } from 'react-icons/ri';
 
 const LoginForm = ({ formik, isLoading }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <form onSubmit={formik.handleSubmit}>
       <div className="flex flex-col mb-2">
@@ -30,18 +33,26 @@ const LoginForm = ({ formik, isLoading }) => {
         <label htmlFor="description" className="text-md mb-1">
           Password:
         </label>
-        <input
-          type="password"
-          placeholder="Add your password"
-          id="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          className={`outline-none border-2 rounded-md border-black/20 py-1 pl-3 transition-all duration-300 ease-linear focus:border-black/40 placeholder:focus:opacity-0 placeholder:focus:duration-200 placeholder:focus:ease-in ${
-            formik.errors.password &&
-            formik.touched.password &&
-            'border-red-500 placeholder:text-red-500 focus:border-red-500'
-          }`}
-        />
+        <div className="relative">
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder="Add your password"
+            id="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            className={`outline-none w-full border-2 rounded-md border-black/20 py-1 pl-3 transition-all duration-300 ease-linear focus:border-black/40 placeholder:focus:opacity-0 placeholder:focus:duration-200 placeholder:focus:ease-in ${
+              formik.errors.password &&
+              formik.touched.password &&
+              'border-red-500 placeholder:text-red-500 focus:border-red-500'
+            }`}
+          />
+          <div
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <RiEyeOffLine /> : <RiEyeLine />}
+          </div>
+        </div>
         {formik.errors.password && formik.touched.password && (
           <p className="text-red-500 mt-[3px] pl-[5px] text-sm mb-0">
             {formik.errors.password}
